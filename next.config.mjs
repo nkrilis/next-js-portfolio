6007879a404326c5472c7ed7a90af8d31434be5f
Nicholas Krilis <n.krilis@gmail.com>
2025-02-20 00:13:12 -0500

    test

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,5 @@ const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   output: "export",
   reactStrictMode: true,
-  basePath: "/next-js-portfolio",
 };
 export default withMDX(nextConfig);
